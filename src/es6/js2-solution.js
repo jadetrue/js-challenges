7ea4a5de1069d981133f9545575f918ee619cfe9
liam <liam@awaitingimage.com>
2019-03-17 20:21:56 +0000

    added in the spread operator

--- src/es6/js2-solution.js
+++ src/es6/js2-solution.js
@@ -2,7 +2,7 @@
 const isPrimary = (color) => (color == "red") || (color == "yellow") || (color == "blue");
 
 export const fixColors = (colorArray, index) => {
-    let clonedArray = colorArray.slice(0);
+    let clonedArray = [...colorArray];
     !isPrimary(clonedArray[index]) ? clonedArray[index] = "red" : null;
     return clonedArray;
 };
