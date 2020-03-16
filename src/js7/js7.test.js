e0b0ef7ca01b83a513486f9ca47429aa2745789f
Liam Farrell <Liam.Farrell@nology.io>
2020-03-16 15:27:34 +0000

    filter challenge

--- src/js7/js7.test.js
+++ src/js7/js7.test.js
@@ -1,1 +1,17 @@
 import challenge from './index';
+
+const words = ["madam", "sir", "mam", "123", "anna"];
+const words2 = ["hello", "Goodbye"];
+const words3 = ["hello", "Goodbye", "noon"];
+
+test("Check pallindromes can be detected", () => {
+  expect(challenge.palindromeChecker("noon")).toBe(true);
+  expect(challenge.palindromeChecker("town")).toBe(false);
+  expect(challenge.palindromeChecker("")).toBe(true);
+})
+
+test("Retrieve all pallindromes in a word list", () => {
+  expect(challenge.getAllPalindromes(words)).toMatchObject(["madam", "mam", "anna"]);
+  expect(challenge.getAllPalindromes(words2)).toMatchObject([]);
+  expect(challenge.getAllPalindromes(words3)).toMatchObject(["noon"]);
+});
--- src/js7/solution.js
+++ src/js7/solution.js
@@ -1,1 +1,25 @@
 /* Array iterators */
+
+/**
+* A function that takes a string and checks to see if it is a palindrome (noon, anna, madam are all palindromes)
+* returns true or false
+*
+* @param {string} word - the word to be checked
+* @returns {boolean} true if the word is a palindrome, false otherwise
+*/
+export const palindromeChecker = (word) => {
+    const reverseWord = word.split("").reverse().join("");
+    return reverseWord === word
+}
+
+/**
+ * A function that takes in an array of words, checks if each word is a palindrome and returns
+ * a new array containing only the words that are palindromes.
+ * You should use an array iterator method, not a for loop!
+ *
+ * @param {array} words - the words to be checked
+ * @returns {array} List of all the words that are palindromes
+ */
+export const getAllPalindromes = (words) => {
+    return words.filter(palindromeChecker);
+}
