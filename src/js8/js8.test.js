470084b130fede1de59af7173824b31d40d7b19c
liam <liam@awaitingimage.com>
2020-04-10 14:06:18 +0100

    updates

--- src/js8/js8.test.js
+++ src/js8/js8.test.js
@@ -1,1 +1,19 @@
-import challenge from './index';
+import challenge from "./index";
+import { person1, person2, person3 } from "./data";
+
+test("Check if People are rich", () => {
+  expect(challenge.isRich(person1)).toBe(false);
+  expect(challenge.isRich(person2)).toBe(true);
+});
+
+test("If two peoples eye colours match", () => {
+  expect(challenge.matchingEyeColour(person1, person2)).toBe(true);
+  expect(challenge.matchingEyeColour(person1, person3)).toBe(false);
+});
+
+test("A year has passed, update the person data", () => {
+  const person1Copy = { ...person1 };
+  challenge.yearPassed(person1Copy);
+  expect(person1).not.toMatchObject(person1Copy);
+  expect(person1Copy.age).toBe(person1.age + 1);
+});
--- src/js8/solution.js
+++ src/js8/solution.js
@@ -1,1 +1,34 @@
-/* Objects */
+/* Objects - See the data.js file for the objects used in the tests */
+
+/**
+ * A function that takes in a person object and will work out if they are rich ( > 1000 per annum)
+ *
+ * @param {object} person - A person object e.g. {name: "Elizabeth Bennet"}
+ * @return {boolean} true if they are rich, false otherwise
+ */
+export const isRich = person => {
+  return person.incomePerAnnum > 1000;
+};
+
+/**
+ * A function that takes in two people objects and checks to see if their eye colours match
+ *
+ * @param {object} person1 - A person object e.g. {name: "Elizabeth Bennet"}
+ * @param {object} person2 - A person object e.g. {name: "Elizabeth Bennet"}
+ * @return {boolean} true if they match
+ */
+export const matchingEyeColour = (person1, person2) => {
+  return person1.eyeColour == person2.eyeColour;
+};
+
+/**
+ * A function that takes a person and increments their age by 1 and calls the calculateFinancesYearEnd function for them
+ *
+ * @param {object} person - A person object e.g. {name: "Elizabeth Bennet"}
+ * @return {object} the updated person
+ */
+export const yearPassed = person => {
+  person.age++;
+  person.calculateFinancesYearEnd();
+  return person;
+};
