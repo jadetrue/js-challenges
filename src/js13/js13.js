4ed154d63589bcce3f61dbcd5a321992df6d988f
Matthew Bickell <matthew@nology.io>
2020-05-05 09:54:54 +0100

    setup done

---
+++ src/js13/js13.js
@@ -0,0 +1,3 @@
+/* Unit testing - In this chalenge you will be provided with some functions that don't have any corrosponding tests */
+/* The aim of this challenge is to achieve 100% code coverage, you can chekck this by running the command below */
+/****** yarn test-coverage ******/
