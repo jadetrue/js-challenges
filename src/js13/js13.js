95fdb09c93b68c607274ba51f86934008a1c8c60
Matthew Bickell <matthew@nology.io>
2020-05-06 16:54:44 +0100

    fixed spelling error

--- src/js13/js13.js
+++ src/js13/js13.js
@@ -1,5 +1,5 @@
 /* Unit testing - In this chalenge you will be provided with some functions that don't have any corrosponding tests */
-/* The aim of this challenge is to achieve 100% code coverage, you can chekck this by running the command below */
+/* The aim of this challenge is to achieve 100% code coverage, you can check this by running the command below */
 /****** yarn test-coverage ******/
 
 /**
