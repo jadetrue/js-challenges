3f74bf99baa182d1a130d7e5bb415232a5be6155
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-01-20 17:41:13 +0000

    (feat): Revamping test Added 4 categories to challenges and added tests

--- src/js3-arrays-and-for-loops/new-challenge.js
+++ src/js3-arrays-and-for-loops/new-challenge.js
@@ -1,3 +1,125 @@
-/**
- * Push and pop
+/* JS3 builds on the previous challenges and adds the use of Arrays, For Loops, Conditionals (If, else, switch)
+ *  & calling your own functions.
  */
+
+/* 
+  All challenges in this repository are seperated into four levels: Foundation, Intermediate, Advanced and Expert.
+  The expectation is to complete all Foundation level challenges, with Intermediate and upwards pushing your knowledge
+  and may require you to google things in order to solve them. If you find an answer online somewhere, be kind and
+  share it with the group!
+*/
+
+/**
+ * Foundation Challenges
+ */
+
+/**
+ * A function that creates a sandwich from an array of ingredients. Add "Bread" to the start and the end.
+ *
+ * @return {array} e.g. (["Bacon","Lettuce","Tomato"]) => ["Bread","Bacon","Lettuce","Tomato","Bread"]
+ */
+
+export const createSandwich = (array) => {
+return
+};
+
+/**
+ * A function that takes Array of Items and returns a NEW ARRAY with the first and last item in it.
+ *
+ * @return {array} e.g (["Tony","John","Dave"]) => ["Tony","Dave"]
+ */
+export const getFirstAndLastItems = (array) => {
+  return 
+};
+
+/**
+ * A function that takes an array of scores and totals the scores by looping through the array.
+ *
+ * @return {number} e.g ([1,2,3]) => 6
+ */
+export const totalScores = (array) => {
+  return 
+};
+
+/**
+ * Intermediate Challenges
+ */
+/**
+ * A function that takes an number and returns the total of the range of numbers between 0 and the given number.
+ *
+ * @return {number} e.g. (10) => 0+1+2+3+4+5+6+7+8+9+10 => 55
+ */
+export const totalRange = (number) => {
+  return 
+};
+
+/**
+ * A function that takes an array and returns a NEW ARRAY where the last item has been moved to the front of the array and removed from the back.
+ *
+ * @return {array} e.g. (["Tony","John","Dave"]) => ["Dave","Tony","John"]
+ */
+export const moveFirstAndLastItems = (array) => {
+  
+  return 
+};
+
+/**
+ * Read this article on how to clone an array.
+ * https://www.samanthaming.com/tidbits/35-es6-way-to-clone-an-array/
+ *
+ * From here on you should not be mutating the input array.Clone it first and then mutate.
+ */
+
+/**
+ * A function that takes an array of numbers and returns a NEW ARRAY with only the odd numbers from the given array. It should not mutate the input array.
+ *
+ * @return {number} e.g. ([1,1,8,1,1,8]) => [1,1,1,1]
+ */
+export const removeEvenNumbers = (array) => {
+  return 
+};
+
+/**
+ * Advanced Challenges
+ */
+
+/**
+ * A function that takes an array of numbers. It returns the average from the given array.
+ * The result should rounded up to the nearest decimal place.
+ *
+ *
+ * @return {number} e.g. ([1,2,3]) => 2
+ */
+export const generateAverage = (array) => {
+  return
+};
+
+/**
+ * A function that uses a loop to reverse the order of an Array. It should return a NEW ARRAY and not Mutate the orginal array.
+ *
+ * @return {number} e.g. ([1,2,3]) => [3,2,1]
+ */
+export const reverseOrder = (array) => {
+  return 
+};
+
+/**
+ * Expert Challenges
+ */
+
+/**
+ * Given two arrays, The first being an array of players and the second being there corresponding score. Loop through them and generate a new array matching the format below.
+ *
+ * ["P:INDEX PLAYER scored HIGHSCORE","P:INDEX PLAYER scored HIGHSCORE","P:INDEX PLAYER scored HIGHSCORE"]
+ *
+ *
+ * P:INDEX will start from 1. e.g P:1 not P:0
+ *
+ * If the inputs are not the same size or empty return "invalid inputs"
+ *
+ ** @return {array} e.g. (["Tony","John","Dave"],[45,55,66]) => ["P:1 Dave scored 45","P:2 Tony scored 55","P:3 John scored 66"]
+ */
+export const generateHighscores = (players, highscores) => {
+  return 
+};
+
--- src/js3-arrays-and-for-loops/solution.js
+++ src/js3-arrays-and-for-loops/solution.js
@@ -1,80 +1,158 @@
-/* JS3 builds on the previous challenges and adds the use of Conditionals (If, else, switch)
+/* JS3 builds on the previous challenges and adds the use of Arrays, For Loops, Conditionals (If, else, switch)
  *  & calling your own functions.
  */
 
+/* 
+  All challenges in this repository are seperated into four levels: Foundation, Intermediate, Advanced and Expert.
+  The expectation is to complete all Foundation level challenges, with Intermediate and upwards pushing your knowledge
+  and may require you to google things in order to solve them. If you find an answer online somewhere, be kind and
+  share it with the group!
+*/
+
 /**
- * A function that takes two numbers and returns the largest of the two numbers
- *
- * @return {number} largest number - e.g. 10
+ * Foundation Challenges
  */
-export const findLargerOfTwoNumbers = (num1, num2) => {
-  if (num1 >= num2) return num1;
-  return num2;
+
+/**
+ * A function that creates a sandwich from an array of ingredients. Add "Bread" to the start and the end.
+ *
+ * @return {array} e.g. (["Bacon","Lettuce","Tomato"]) => ["Bread","Bacon","Lettuce","Tomato","Bread"]
+ */
+
+export const createSandwich = (ingredients) => {
+  return ["Bread", ...ingredients, "Bread"];
 };
 
 /**
- * A function that takes three numbers and returns the smallest of the three numbers
+ * A function that takes Array of Items and returns a NEW ARRAY with the first and last item in it.
  *
- * @return {number} smallest number - e.g. 10
+ * @return {array} e.g (["Tony","John","Dave"]) => ["Tony","Dave"]
  */
-export const findSmallestOfThreeNumbers = (num1, num2, num3) => {
-  if (num1 <= num2 && num1 <= num3) return num1;
-  if (num2 <= num1 && num2 <= num3) return num2;
-  return num3;
+export const getFirstAndLastItems = (items) => {
+  const first = items[0];
+  const last = items[items.length - 1];
+  return [first, last];
 };
 
 /**
- * A function that takes a number from 0 - 100 and returns a letter grade
- * This function can also handle incorrect input data and returns "Not a valid score!"
- * 0 - 69 is D
- * 70 - 79 is C
- * 80 - 89 is B
- * 90 - 100 is A
+ * A function that takes an array of scores and totals the scores by looping through the array.
  *
- * @return {string} grade - The relevant grade for the score e.g. A
+ * @return {number} e.g ([1,2,3]) => 6
  */
-export const calculateGrade = (score) => {
-  if (typeof score != "number" || score < 0) return "Not a valid score!";
-  if (score < 70) {
-    return "D";
-  } else if (score < 80) {
-    return "C";
-  } else if (score < 90) {
-    return "B";
-  } else {
-    return "A";
+export const totalScores = (score) => {
+  let total = 0;
+  for (let index = 0; index < score.length; index++) {
+    total += score[index];
   }
+  return total;
 };
 
 /**
- * A function that takes a persons name and a score from 0 - 100
- * It generates a greeting for a report card based on the score
- * 0 - 69 is D "Sorry NAME, you received a failing grade of D"
- * 70 - 79 is C "Dear NAME, you received a grade of C"
- * 80 - 89 is B "Well done NAME, you received a grade of B"
- * 90 - 100 is A "Well done NAME! you received the highest grade of A"
- * Error - "Sorry NAME, we were unable to proccess your score results"
- *
- * @return {string} grade - The relevant grade for the score e.g. A
+ * Intermediate Challenges
  */
-export const generateReportCardGreeting = (name, score) => {
-  let greeting = "";
-  let grade = calculateGrade(score);
-  switch (grade) {
-    case "D":
-      greeting = "Sorry " + name + ", you received a failing grade of D";
-      break;
-    case "C":
-      greeting = "Dear " + name + ", you received a grade of C";
-      break;
-    case "B":
-      greeting = "Well done " + name + ", you received a grade of B";
-      break;
-    case "A":
-      greeting = "Well done " + name + "! you received the highest grade of A";
-      break;
-    default:
-      greeting = "Sorry " + name + ", we were unable to process your score results";
+/**
+ * A function that takes an number and returns the total of the range of numbers between 0 and the given number.
+ *
+ * @return {number} e.g. (10) => 0+1+2+3+4+5+6+7+8+9+10 => 55
+ */
+export const totalRange = (number) => {
+  let total = 0;
+  for (let index = 0; index <= number; index++) {
+    total += index;
   }
-  return greeting;
+  return total;
 };
+
+/**
+ * A function that takes an array and returns a NEW ARRAY where the last item has been moved to the front of the array and removed from the back.
+ *
+ * @return {array} e.g. (["Tony","John","Dave"]) => ["Dave","Tony","John"]
+ */
+export const moveFirstAndLastItems = (items) => {
+  const result = [...items];
+  const lastItem = result.pop();
+  result.unshift(lastItem);
+  return result;
+};
+
+/**
+ * Read this article on how to clone an array.
+ * https://www.samanthaming.com/tidbits/35-es6-way-to-clone-an-array/
+ *
+ * From here on you should not be mutating the input array.Clone it first and then mutate.
+ */
+
+/**
+ * A function that takes an array of numbers and returns a NEW ARRAY with only the odd numbers from the given array. It should not mutate the input array.
+ *
+ * @return {number} e.g. ([1,1,8,1,1,8]) => [1,1,1,1]
+ */
+export const removeEvenNumbers = (array) => {
+  const oddNumbers = [];
+  for (let index = 0; index < array.length; index++) {
+    let current = array[index];
+    if (current % 2 !== 0) {
+      oddNumbers.push(current);
+    }
+  }
+  return oddNumbers;
+};
+
+/**
+ * Advanced Challenges
+ */
+
+/**
+ * A function that takes an array of numbers. It returns the average from the given array.
+ * The result should rounded up to the nearest decimal place.
+ *
+ *
+ * @return {number} e.g. ([1,2,3]) => 2
+ */
+export const generateAverage = (array) => {
+  const total = totalScores(array);
+  const average = total / array.length;
+  return Math.round(average) || 0;
+};
+
+/**
+ * A function that uses a loop to reverse the order of an Array. It should return a NEW ARRAY and not Mutate the orginal array.
+ *
+ * @return {number} e.g. ([1,2,3]) => [3,2,1]
+ */
+export const reverseOrder = (array) => {
+  const reversed = [];
+  for (let index = array.length - 1; index >= 0; index--) {
+    reversed.push(array[index]);
+  }
+  return reversed;
+};
+
+/**
+ * Expert Challenges
+ */
+
+/**
+ * Given two arrays, The first being an array of players and the second being there corresponding score. Loop through them and generate a new array matching the format below.
+ *
+ * ["P:INDEX PLAYER scored HIGHSCORE","P:INDEX PLAYER scored HIGHSCORE","P:INDEX PLAYER scored HIGHSCORE"]
+ *
+ *
+ * P:INDEX will start from 1. e.g P:1 not P:0
+ *
+ * If the inputs are not the same size or empty return "invalid inputs"
+ *
+ ** @return {array} e.g. (["Tony","John","Dave"],[45,55,66]) => ["P:1 Dave scored 45","P:2 Tony scored 55","P:3 John scored 66"]
+ */
+export const generateHighscores = (players, highscores) => {
+  if (players.length !== highscores.length || !players.length) return "invalid inputs";
+  const scores = [];
+  while (scores.length !== players.length) {
+    const index = scores.length;
+    const message = `P:${index + 1} ${players[index]} scored ${highscores[index]}`;
+    scores.push(message);
+  }
+
+  return scores;
+};
+
