eefe74106834b2f76fb15e2040540c83f82e5cea
Matthew Bickell <matthew@nology.io>
2021-01-25 11:14:18 +0000

    setup for writing challenges

---
+++ src/js6-objects/index.js
@@ -0,0 +1,12 @@
+// This file allows us to dynamically export either the solution or challenge file
+// depending on the arguments used to run the test from the command line
+
+import * as challenge from "./solution";
+
+let exportedModule = challenge;
+
+if (typeof __CHALLENGE__ !== "undefined" && __CHALLENGE__) {
+  exportedModule = challenge;
+}
+
+export default exportedModule;
