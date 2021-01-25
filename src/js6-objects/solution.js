8bb1729e91b4d4dcd8e827fa9a674254893aee2f
Matthew Bickell <matthew@nology.io>
2021-01-25 12:23:15 +0000

    added first challenge

--- src/js6-objects/solution.js
+++ src/js6-objects/solution.js
@@ -1,63 +1,35 @@
-/* Functions as arguments (callbacks) */
+/* This challenge build upon previous knowledge and introduces the use of objects */
+
+/* 
+  All challenges in this repository are seperated into four levels: Foundation, Intermediate, Advanced and Expert.
+  The expectation is to complete all Foundation level challenges, with Intermediate and upwards pushing your knowledge
+  and may require you to google things in order to solve them. If you find an answer online somewhere, be kind and
+  share it with the group!
+*/
+
+/* Foundation Challenges */
 
 /**
-* A function that takes a string and checks to see if it is a palindrome (noon, anna, madam are all palindromes)
-* returns true or false
-*
-* @param {string} word - the word to be checked
-* @returns {boolean} true if the word is a palindrome, false otherwise
-*/
-export const palindromeChecker = (word) => {
-  const reverseWord = word.split("").reverse().join("");
-  return reverseWord === word
-}
+ * A function that takes a furniture object and returns its price
+ *
+ * furniture = {
+ *  name: "lack",
+ *  price: 6
+ * }
+ *
+ * @param {object} furniture - A piece of furniture from the catalogue
+ * @param {string} furniture.name - The name of the furniture
+ * @param {number} furniture.price - The price of the furniture
+ * @return {number} The price of the piece of furniture
+ */
+export const getFurniturePrice = (furniture) => {
+  const price = furniture.price;
 
-/**
-* A function that takes a string and checks to see if it contains vowels
-* returns true or false
-*
-* @param {string} word - the word to be checked
-* @returns {boolean} true if the word contains vowels, false otherwise
-*/
-export const vowelChecker = (word) => {
-  return /[a,e,i,o,u]/.test(word);
-}
+  return price;
+};
 
-/**
-* A function that takes a word and a function as arguments
-* First check to see if the word is a valid string, it contains atleast 1 letter and is exactly 1 word, not two or more
-* If the word is a valid single word, call the provided function with the word and return the output
-* If not a valid single word then return 'Input must be a valid single word'
-*
-* @param {string} word - the word to be checked
-* @param {function} wordCheckCallback - A function to execute with a valid word
-* @returns {boolean | string} true or false for a valid word
-*/
-export const useWordCheckCallback = (word, wordCheckCallback) => {
- if (typeof word === "string" && word !== "" && word.indexOf(" ") < 0 ){
-   return wordCheckCallback(word);
- }
- return "Input must be a valid single word";
-}
+/* Intermediate Challenges */
 
-/**
-* A function that takes an array of words and a function as arguments
-* Check each word to see if it is a valid string, it contains atleast 1 letter and is exactly 1 word, not two or more
-* If the word is a valid single word, call the provided function with the word
-* for all valid words that pass the function, add to an new array and return that array
-*
-* @param {array} words - the words to be checked
-* @param {function} wordCheckCallback - A function to execute with a valid word
-* @returns {array} list of all valid words that pass the check
-*/
-export const checkWords = (words, wordCheckCallback) => {
-  let validWords = [];
-  for (let i = 0; i < words.length; i++){
-    let result = useWordCheckCallback(words[i], wordCheckCallback);
-    if (result && typeof result === "boolean"){
-      validWords.push(words[i]);
-    }
-  }
-  return validWords;
-}
+/* Advanced Challenges */
 
+/* Expert Challenge */
