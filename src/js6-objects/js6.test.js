458845b11609dbf5908194ee0b4d151947969f78
Matthew Bickell <matthew@nology.io>
2021-01-26 12:16:24 +0000

    finished off foundation and intermediate challenges

--- src/js6-objects/js6.test.js
+++ src/js6-objects/js6.test.js
@@ -1,6 +1,13 @@
 import challenge from "./index";
 
-const { getFurniturePrice } = challenge;
+const {
+  getFurniturePrice,
+  addFurnitureStoreLocation,
+  makeSpaceship,
+  addUserName,
+  splitFullNameToFirstAndLast,
+  accessGivenKey
+} = challenge;
 
 describe("getFurniturePrice() tests", () => {
   const table = { name: "lack", price: 6 };
@@ -9,5 +16,115 @@ describe("getFurniturePrice() tests", () => {
   it("Should return the correct price", () => {
     expect(getFurniturePrice(table)).toBe(6);
     expect(getFurniturePrice(cabinet)).toBe(55);
+  });
+});
+
+describe("addFurnitureStorLocation() tests", () => {
+  const table = { name: "lack", price: 6 };
+  const cabinet = { name: "brimnes", price: 55 };
+
+  it("Should return an object", () => {
+    expect(typeof addFurnitureStoreLocation(table, "Bristol")).toBe("object");
+  });
+
+  const tableWithLocation = addFurnitureStoreLocation(table, "Bristol");
+  const cabinetWithLocation = addFurnitureStoreLocation(cabinet, "Cardiff");
+
+  it("Should add a property of location", () => {
+    expect(tableWithLocation).toHaveProperty("location");
+    expect(cabinetWithLocation).toHaveProperty("location");
+  });
+
+  it("Should still have all of the old properties", () => {
+    expect(tableWithLocation).toHaveProperty("name");
+    expect(tableWithLocation).toHaveProperty("price");
+    expect(cabinetWithLocation).toHaveProperty("name");
+    expect(cabinetWithLocation).toHaveProperty("price");
+  });
+
+  it("Should exactly match given object", () => {
+    expect(tableWithLocation).toMatchObject({ name: "lack", price: 6, location: "Bristol" });
+    expect(cabinetWithLocation).toMatchObject({ name: "brimnes", price: 55, location: "Cardiff" });
+  });
+});
+
+describe("makeSpaceship() tests", () => {
+  it("Should return an object", () => {
+    expect(typeof makeSpaceship("rocinante", 8, "epstein drive", true)).toBe("object");
+  });
+
+  const rocinante = { name: "rocinante", noOfSeats: 8, engineType: "epstein drive", canTravelSolarSystems: true };
+  const enterprise = {
+    name: "uss enterprise (ncc-1701-d)",
+    noOfSeats: 5,
+    engineType: "impulse drive",
+    canTravelSolarSystems: true
+  };
+  it("Should create an object with the provided arguments as values", () => {
+    expect(makeSpaceship("rocinante", 8, "epstein drive", true)).toMatchObject(rocinante);
+    expect(makeSpaceship("uss enterprise (ncc-1701-d)", 5, "impulse drive", true)).toMatchObject(enterprise);
+  });
+});
+
+describe("addUserName() tests", () => {
+  const user1 = { name: "john smith", username: "johnnyboiii" };
+  const user2 = { name: "jane doe", username: "killerZ" };
+  it("Should return and object", () => {
+    expect(typeof addUserName(user1, "XXjohnyboiXX")).toBe("object");
+  });
+
+  it("Shouldn't change the username if one already exists", () => {
+    expect(addUserName(user1, "XXjohnyboiXX").username).toBe("johnnyboiii");
+    expect(addUserName(user1, "XXjohnyboiXX")).toMatchObject(user1);
+    expect(addUserName(user2, "butterflyflowerqueen").username).toBe("killerZ");
+    expect(addUserName(user2, "butterflyflowerqueen")).toMatchObject(user2);
+  });
+
+  const user3 = { name: "paul blart" };
+  const user4 = { name: "gimli son of gloin" };
+  it("Should change the username if none exists", () => {
+    expect(addUserName(user3, "mallcop12").username).toBe("mallcop12");
+    expect(addUserName(user3, "mallcop12")).toMatchObject({ name: "paul blart", username: "mallcop12" });
+    expect(addUserName(user4, "ih8elves").username).toBe("ih8elves");
+    expect(addUserName(user4, "ih8elves")).toMatchObject({ name: "gimli son of gloin", username: "ih8elves" });
+  });
+});
+
+describe("splitFullNameToFirstAndLast() tests", () => {
+  const customer1 = { fullName: "Barry Chuckle" };
+  const customer2 = { fullName: "Paul Chuckle" };
+
+  it("Should split up basic names", () => {
+    expect(splitFullNameToFirstAndLast(customer1)).toMatchObject({
+      fullName: "Barry Chuckle",
+      firstName: "Barry",
+      lastName: "Chuckle"
+    });
+
+    expect(splitFullNameToFirstAndLast(customer2)).toMatchObject({
+      fullName: "Paul Chuckle",
+      firstName: "Paul",
+      lastName: "Chuckle"
+    });
+  });
+});
+
+describe("accessGivenKey() tests", () => {
+  const person = { name: "Joe Bloggs", height: 170, eyeColour: "blue", likesIceCream: false };
+
+  it("Should access the eyeColour property", () => {
+    expect(accessGivenKey(person, "eyeColour")).toBe("blue");
+  });
+
+  it("Should access the height property", () => {
+    expect(accessGivenKey(person, "height")).toBe(170);
+  });
+
+  it("Should access the name property", () => {
+    expect(accessGivenKey(person, "name")).toBe("Joe Bloggs");
+  });
+
+  it("Should access the likesIceCream property", () => {
+    expect(accessGivenKey(person, "likesIceCream")).toBe(false);
   });
 });
--- src/js6-objects/solution.js
+++ src/js6-objects/solution.js
@@ -10,7 +10,7 @@
 /* Foundation Challenges */
 
 /**
- * A function that takes a furniture object and returns its price
+ * A function that takes a furniture object from the catalogue and returns its price
  *
  * furniture = {
  *  name: "lack",
@@ -28,8 +28,95 @@ export const getFurniturePrice = (furniture) => {
   return price;
 };
 
+/**
+ * A function to attach to a store location to a furniture object from the catalogue
+ *
+ * @param {object} furniture - A piece of furniture from the catalogue
+ * @param {string} furniture.name - The name of the furniture
+ * @param {number} furniture.price - The price of the furniture
+ * @param {string} location - A store location to attach to a piece of furniture
+ * @returns {{name: string, price: number, location: string}} furniture - A furniture object from the catalogue
+ */
+export const addFurnitureStoreLocation = (furniture, location) => {
+  furniture.location = location;
+
+  return furniture;
+};
+
+/**
+ * A function which takes a selection of arguments relating to a space ship and uses them
+ * to create a space ship object
+ *
+ * @param {string} name The name of the space ship
+ * @param {number} noOfSeats The number of seat of the space ship
+ * @param {string} engineType The type of engine in the space ship
+ * @param {boolean} canTravelSolarSystems The ability for the space ship to travel to different solar systems
+ * @returns {{name: string, noOfSeats: number, engineType: string, canTravelSolarSystems: boolean}} spaceship - The space ship object
+ */
+export const makeSpaceship = (name, noOfSeats, engineType, canTravelSolarSystems) => {
+  const spaceship = {
+    name: name,
+    noOfSeats: noOfSeats,
+    engineType: engineType,
+    canTravelSolarSystems: canTravelSolarSystems
+  };
+
+  return spaceship;
+};
 /* Intermediate Challenges */
 
+/**
+ * A function that will attach a username to a user but only if the user doesn't already have one
+ *
+ * @param {object} user - A user object
+ * @param {string} user.name - The users name
+ * @param {string} [user.username] - The users username (OPTIONAL)
+ * @param {string} username - A username to attach
+ * @returns {{name: string, username: string}} User - The user object with the same username or a new one
+ */
+export const addUserName = (user, username) => {
+  const hasUserName = user.hasOwnProperty("username");
+
+  if (!hasUserName) {
+    user.username = username;
+  }
+
+  return user;
+};
+
+/**
+ * A function which takes a customer object from the database and returns the same object where the name has been
+ * split into first and last name and reattached to the object
+ *
+ * @param {object} customer A customer object from the database
+ * @param {string} customer.fullName - The cutsomers full name
+ * @returns {{fullName: string, firstName: string, lastName: string}} A customer object from the database with the name separated into first and last
+ */
+export const splitFullNameToFirstAndLast = (customer) => {
+  const fullName = customer.fullName;
+  const splitNames = fullName.split(" ");
+  const firstName = splitNames[0];
+  const lastName = splitNames[1];
+
+  customer.firstName = firstName;
+  customer.lastName = lastName;
+  return customer;
+};
+
+/**
+ * A function which access a given key on an object
+ *
+ * @param {object} object An object with some key value pairs (Not important what they are)
+ * @param {string} key A key to access on the object
+ * @returns {any} value - The value you have accessed on the object
+ */
+export const accessGivenKey = (object, key) => {
+  const value = object[key];
+
+  return value;
+};
 /* Advanced Challenges */
+
+// Do some arrays on objects / nested objects stuff here
 
 /* Expert Challenge */
