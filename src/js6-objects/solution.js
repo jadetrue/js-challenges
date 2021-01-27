a22ebf6360c8deca1acabbade609cc335b94719b
Matthew Bickell <matthew@nology.io>
2021-01-27 15:18:35 +0000

    Changed jsDoc implementation

--- src/js6-objects/solution.js
+++ src/js6-objects/solution.js
@@ -17,9 +17,7 @@
  *  price: 6
  * }
  *
- * @param {object} furniture - A piece of furniture from the catalogue
- * @param {string} furniture.name - The name of the furniture
- * @param {number} furniture.price - The price of the furniture
+ * @param {{name: string, price: number}} furniture - A piece of furniture from the catalogue
  * @return {number} The price of the piece of furniture
  */
 export const getFurniturePrice = (furniture) => {
@@ -31,9 +29,7 @@ export const getFurniturePrice = (furniture) => {
 /**
  * A function to attach to a store location to a furniture object from the catalogue
  *
- * @param {object} furniture - A piece of furniture from the catalogue
- * @param {string} furniture.name - The name of the furniture
- * @param {number} furniture.price - The price of the furniture
+ * @param {{name: string, price: number}} furniture - A piece of furniture from the catalogue
  * @param {string} location - A store location to attach to a piece of furniture
  * @returns {{name: string, price: number, location: string}} furniture - A furniture object from the catalogue
  */
@@ -68,9 +64,7 @@ export const makeSpaceship = (name, noOfSeats, engineType, canTravelSolarSystems
 /**
  * A function that will attach a username to a user but only if the user doesn't already have one
  *
- * @param {object} user - A user object
- * @param {string} user.name - The users name
- * @param {string} [user.username] - The users username (OPTIONAL)
+ * @param {{name: string, username?: string}} user - A user object
  * @param {string} username - A username to attach
  * @returns {{name: string, username: string}} User - The user object with the same username or a new one
  */
@@ -88,8 +82,7 @@ export const addUserName = (user, username) => {
  * A function which takes a customer object from the database and returns the same object where the name has been
  * split into first and last name and reattached to the object
  *
- * @param {object} customer A customer object from the database
- * @param {string} customer.fullName - The cutsomers full name
+ * @param {{fullName: string,}} customer A customer object from the database
  * @returns {{fullName: string, firstName: string, lastName: string}} A customer object from the database with the name separated into first and last
  */
 export const splitFullNameToFirstAndLast = (customer) => {
@@ -124,14 +117,7 @@ export const accessGivenKey = (object, key) => {
  * A function which takes a user object, accesses it's address and returns it as a string to print onto a shipping label
  * "4 Privet Drive Little Whinging Surrey CR3 0AA"
  *
- * @param {object} user - A user object from the database
- * @param {number} user.id - The user ID
- * @param {string} user.name - The users name
- * @param {object} user.address - The users address
- * @param {string} user.address.line1 - Line 1 of the address
- * @param {string} user.address.line2 - Line 2 of the address
- * @param {string} user.address.city - City of the address
- * @param {string} user.address.postcode - postcode of the address
+ * @param {{id: number, name: string, address: {line1: string, line2: string, city: string, postcode: string}}} user - A user object from the database
  * @returns {string} An address string for a shipping label
  */
 export const getUserAddress = (user) => {
@@ -144,10 +130,7 @@ export const getUserAddress = (user) => {
  * A function that given a customer for the restaurant with a list of known allergies and a list of allergens in an
  * array, will attach an array of allergens safe for the customer to eat to the customer object and return it
  *
- * @param {object} customer - A customer for the restaurant
- * @param {number} customer.id - The id of the customer
- * @param {string} customer.name - The name of the customer
- * @param {string[]} customer.allergies - The things the customer is allergic to
+ * @param {{id: number, name: string, allergies: string[]}} customer - A customer for the restaurant
  * @param {string[]} allergenList - A list of all known allergens
  * @return {{id: number, name: string, allergies: string[], safeAllergens: string[]}} customer
  */
