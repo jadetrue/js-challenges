38a96adf85c545e253f5aee6024cdc0e4415f6dd
Matthew Bickell <matthew@nology.io>
2021-01-28 12:14:06 +0000

    setup done

---
+++ src/complex-data-types/solution.js
@@ -0,0 +1,16 @@
+/* This challenge build upon previous knowledge and introduces the concept of complex data types (arrays of objects) */
+
+/* 
+  All challenges in this repository are seperated into four levels: Foundation, Intermediate, Advanced and Expert.
+  The expectation is to complete all Foundation level challenges, with Intermediate and upwards pushing your knowledge
+  and may require you to google things in order to solve them. If you find an answer online somewhere, be kind and
+  share it with the group!
+*/
+
+/* Foundation Challenges */
+
+/* Intermediate Challenges */
+
+/* Advanced Challenges */
+
+/* Expert Challenge */
